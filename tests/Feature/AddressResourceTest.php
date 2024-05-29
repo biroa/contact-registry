3d2687f907ca17e090665532ee1237e86d653740
Adam <adam.biro@gmail.com>
2024-05-29 10:14:00 +0200

    * Add test_addresses_show_resource test to AddressResourceTest.php

--- tests/Feature/AddressResourceTest.php
+++ tests/Feature/AddressResourceTest.php
@@ -2,11 +2,31 @@
 
 namespace Tests\Feature;
 
+use App\Models\Address;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class AddressResourceTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function test_addresses_show_resource(): void
+    {
+        $address = Address::factory()->create();
+        $response = $this->get('/api/addresses/'. $address->id);
+        $response->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->hasAll(
+                [
+                    'data.id',
+                    'data.country',
+                    'data.settlement',
+                    'data.street',
+                    'data.streetNumber',
+                    'data',
+                    'meta',
+                    'meta.message'
+                ]));
+    }
 
 }
